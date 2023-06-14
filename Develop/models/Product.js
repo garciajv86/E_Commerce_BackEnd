3583c7af41e53a905bb43c9a79f27855d0fc94a1
Joshua Garcia <112138421+garciajv86@users.noreply.github.com>
2023-06-14 12:29:33 -0500

    Finished Category.js and set up all other models

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,31 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      //Integer
+      //Doesn't allow null values
+      //Set as primary key
+      //Uses auto increment
+    },
+    product_name: {
+      //String.
+      //Doesn't allow null values.
+    },
+    price: {
+      //Decimal.
+      //Doesn't allow null values.
+      //Validates that the value is a decimal.
+    },
+    stock: {
+      //Integer.
+      //Doesn't allow null values
+      //Set a default value of 10
+      //Validates that the value is numeric
+    },
+    category_id: {
+      //Integer
+      //References the Category model's id
+    },
   },
   {
     sequelize,
