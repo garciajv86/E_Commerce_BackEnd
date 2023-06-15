5b7375fbc01078eda89df674043b669411320b80
Joshua Garcia <112138421+garciajv86@users.noreply.github.com>
2023-06-15 12:40:14 -0500

    formatted comments in routes, created db design diagram and added erd.dio to gitignore

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -1,33 +1,33 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
-// The `/api/products` endpoint
+//* The `/api/products` endpoint
 
-// get all products
+//* get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  //* find all products
+  //* be sure to include its associated Category and Tag data
 });
 
-// get one product
+//* get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  //* find a single product by its `id`
+  //* be sure to include its associated Category and Tag data
 });
 
-// create new product
+//* create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+  //*  req.body should look like this...
+  //*   {
+  //*     product_name: "Basketball",
+  //*     price: 200.00,
+  //*     stock: 3,
+  //*     tagIds: [1, 2, 3, 4]
+  //*   }
+  
   Product.create(req.body)
     .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+      //* if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
@@ -37,7 +37,7 @@ router.post('/', (req, res) => {
         });
         return ProductTag.bulkCreate(productTagIdArr);
       }
-      // if no product tags, just respond
+      //* if no product tags, just respond
       res.status(200).json(product);
     })
     .then((productTagIds) => res.status(200).json(productTagIds))
