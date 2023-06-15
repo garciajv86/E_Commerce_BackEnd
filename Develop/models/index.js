ba5dcd946be219bd8ba0b6859b87eebe6f943e00
Joshua Garcia <112138421+garciajv86@users.noreply.github.com>
2023-06-15 11:38:05 -0500

    Completed the models index file

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,13 +4,28 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+//* Product belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  //? Do I need onDelete CASCADE or keep it NULL?
+})
+//* Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+  //? onDelete: 'CASCADE',?
+})
 
-// Categories have many Products
+//* Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
-
-// Tags belongToMany Products (through ProductTag)
+//* Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+})
 
 module.exports = {
   Product,
