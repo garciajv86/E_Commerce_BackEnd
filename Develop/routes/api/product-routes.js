325a6c870b2b61e249e8789e9549dafa62f1e954
Joshua Garcia <112138421+garciajv86@users.noreply.github.com>
2023-06-16 12:05:21 -0500

    finished my post req

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -58,13 +58,7 @@ router.get('/:id', (req, res) => {
 
 //* create new product
 router.post('/', async (req, res) => {
-  //*  req.body should look like this...
-  //*   {
-  //*     product_name: "Basketball",
-  //*     price: 200.00,
-  //*     stock: 3,
-  //*     tagIds: [1, 2, 3, 4]
-  //*   }
+  const { product_name, price, stock, tagIds } = req.body;
   
   Product.create(req.body)
     .then((product) => {
